2359c1087eff0f7437102bfa7d334a030291774d
robledo21 <101658962+robledo21@users.noreply.github.com>
2022-03-15 18:44:35 +0100

    Update Barco.java

--- src/proyecto/Barco.java
+++ src/proyecto/Barco.java
@@ -1,5 +1,49 @@
 package proyecto;
 
-public class Barco {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class Barco
+{
+	
+	private ArrayList<Tupla> info;
+	
+	public Barco()
+	{
+		this.info = new ArrayList<Tupla>();
+	}
+	
+	public boolean atacarBarco(int pX, int pY)  // si las posiciones dadas tocan un barco, devuelve True y pone el atributo tocado del Barco a True.
+	{											// en caso contrario devuelve False y deja el atributo como estaba.
+		boolean haTocado = false;
+		Tupla tuplaTocada = buscarPosiciones(pX, pY);
+		if (tuplaTocada != null)
+		{
+			tuplaTocada.complementarTocado();
+			haTocado = true;
+		}
+		return haTocado;
+	}
+	
+	
+	public Tupla buscarPosiciones(int pX, int pY)  // si la posiciones dadas coinciden con las de una Tupla, la devuelve.
+	{											   // en caso contrario devuelve null.
+		Iterator<Tupla> itr = this.info.iterator();     // pasar a java8 ****
+		boolean enc = false;
+		Tupla unaTupla = null;
+		
+		while (!enc && itr.hasNext())
+		{
+			unaTupla = itr.next();
+			if ((unaTupla.getX() == pX) && (unaTupla.getY() == pY))
+			{
+				enc = true;
+			}
+		}
+		
+		if (enc) {   return unaTupla;  }
+		else	 {   return null;	   }
+		
+	}
+	
 }
