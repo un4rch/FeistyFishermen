4c6052f5fde19a9a5be219e9f321ab8bf2c84fc1
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-03-15 18:33:35 +0100

    Update Controlador.java

--- src/proyecto/Controlador.java
+++ src/proyecto/Controlador.java
@@ -2,4 +2,22 @@ package proyecto;
 
 public class Controlador {
 
+    private static Controlador miControlador;
+    private ArrayList<Barco> listaBarcosJugador;
+    private ArrayList<Barco> listaBarcosEnemigo;
+
+    private Controlador()
+    {
+
+    }
+
+    public static Controlador getMiControlador()
+    {
+        if (Controlador.miControlador == null)
+        {
+            Controlador.miControlador = new Controlador();
+        }
+        return(Controlador.miControlador);
+    }
+
 }
