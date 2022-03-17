81d44f5ea45d037cfacd8368875c4759f5229492
un4rch <81522320+un4rch@users.noreply.github.com>
2022-03-17 18:12:54 +0100

    Cambio

--- src/packModeloControlador/Controlador.java
+++ src/packModeloControlador/Controlador.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Observable;
 
-public class Controlador extends Observable{
+public class Controlador extends Observable {
 
     private static Controlador miControlador;
     private ArrayList<Barco> listaBarcosJugador;
     private ArrayList<Barco> listaBarcosEnemigo;
+    private int tipoBarco; //Longitud del barco
+    private int direccion;
+    private int tipoArma;
 
     private Controlador()
     {
         this.listaBarcosJugador = new ArrayList<Barco>();
         this.listaBarcosEnemigo = new ArrayList<Barco>();
+        this.tipoBarco = 1;
+        this.direccion = 1;
+        this.tipoArma = 1;
     }
 
     public static Controlador getMiControlador()
@@ -35,7 +41,7 @@ public class Controlador extends Observable{
         return this.listaBarcosEnemigo.iterator();
     }
 
-    public boolean verSiHayGanador() //Recorre las 2 listas de barcos y devuelve un booleano que indica si la partida ha terminado, además de indicar por pantalla quién es el ganador
+    public boolean verSiHayGanador() //Recorre las 2 listas de barcos y devuelve un booleano que indica si la partida ha terminado, ademÃ¡s de indicar por pantalla quiÃ©n es el ganador
     {
         boolean ganador = true;
         Iterator<Barco> itr = this.getItrBarcosJugador();
@@ -43,7 +49,7 @@ public class Controlador extends Observable{
         while (itr.hasNext() && ganador)
         {
             unBarco = itr.next();
-            if(!unBarco.estaHundido()) //si un barco no está hundido
+            if(!unBarco.estaHundido()) //si un barco no estÃ¡ hundido
             {
                 ganador = false;
             }
@@ -60,7 +66,7 @@ public class Controlador extends Observable{
             while (itr.hasNext() && ganador)
             {
                 unBarco = itr.next();
-                if(!unBarco.estaHundido()) //si un barco no está hundido
+                if(!unBarco.estaHundido()) //si un barco no estÃ¡ hundido
                 {
                     ganador = false;
                 }
@@ -73,6 +79,37 @@ public class Controlador extends Observable{
         }
 
         return(ganador);    //Devuelve el booleano que indica si ha de terminar la partida
+    }
+    
+    public boolean casillaRivalPulsada(int pos) {
+    	System.out.print(pos);
+    	return true;
+    }
+    
+    public ArrayList<Integer> casillaUsuarioPulsada(int pos) {
+    	ArrayList<Integer> posisBarco = new ArrayList<Integer>();
+    	int x = pos%10;
+    	int y = pos/10;
+    	System.out.println(x+"-"+y);
+    	posisBarco.add(pos);
+    	if (this.direccion == 1) {
+    		for (int i=1; i<=tipoBarco; i++) {
+    			posisBarco.add(x+(10*y+i));
+    		}
+    	} else if (this.direccion == 2) {
+    		for (int i=1; i<=tipoBarco; i++) {
+    			posisBarco.add(x+i+10*y);
+    		}
+    	} else if (this.direccion == 3) {
+    		for (int i=1; i<=tipoBarco; i++) {
+    			posisBarco.add(x+(10*y+i));
+    		}
+    	} else {
+    		for (int i=1; i<=tipoBarco; i++) {
+    			posisBarco.add(x+(10*y+i));
+    		}
+    	}
+    	return posisBarco;
     }
 
 }
