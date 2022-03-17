ba2b72c40103040219b90cd43a7c83855d7dfffa
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-03-17 01:01:51 +0100

    Update Controlador.java

--- src/proyecto/Controlador.java
+++ src/proyecto/Controlador.java
@@ -1,6 +1,7 @@
 package proyecto;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Controlador {
 
@@ -23,10 +24,54 @@ public class Controlador {
         return(Controlador.miControlador);
     }
 
+    private Iterator<Barco> getItrBarcosJugador()
+    {
+        return this.listaBarcosJugador.iterator();
+    }
+
+    private Iterator<Barco> getItrBarcosEnemigo()
+    {
+        return this.listaBarcosEnemigo.iterator();
+    }
+
     public boolean verSiHayGanador() //Recorre las 2 listas de barcos y devuelve un booleano que indica si la partida ha terminado, además de indicar por pantalla quién es el ganador
     {
-        //TODO
-        return(false);
+        boolean ganador = true;
+        Iterator<Barco> itr = this.getItrBarcosJugador();
+        Barco unBarco = null;
+        while (itr.hasNext() && ganador)
+        {
+            unBarco = itr.next();
+            if(!unBarco.estaHundido()) //si un barco no está hundido
+            {
+                ganador = false;
+            }
+        }
+
+        if(ganador)     //Ha ganado el Enemigo
+        {
+            //TODO (Pop-up para el Enemigo)
+        }
+        else            //No ha ganado el enemigo, comprobamos si ha ganado el jugador
+        {
+            ganador = true;     //porque tenemos que volver a comprobar para jugador
+            itr = this.getItrBarcosEnemigo();
+            while (itr.hasNext() && ganador)
+            {
+                unBarco = itr.next();
+                if(!unBarco.estaHundido()) //si un barco no está hundido
+                {
+                    ganador = false;
+                }
+            }
+
+            if(ganador)     //Ha ganado el Jugador
+            {
+                //TODO (Pop-up para el Jugador)
+            }
+        }
+
+        return(ganador);    //Devuelve el booleano que indica si ha de terminar la partida
     }
 
 }
