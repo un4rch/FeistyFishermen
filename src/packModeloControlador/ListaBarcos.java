b1ab785513144ba3978447f2a7458855369cacc6
RubenEsgueva <81328415+RubenEsgueva@users.noreply.github.com>
2022-03-21 18:10:51 +0100

    Cambios 21/03

--- src/packModeloControlador/ListaBarcos.java
+++ src/packModeloControlador/ListaBarcos.java
@@ -1,6 +1,7 @@
 package packModeloControlador;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ListaBarcos {
 	private ArrayList<Barco> listaBarcos;
@@ -9,99 +10,118 @@ public class ListaBarcos {
 		this.listaBarcos = new ArrayList<Barco>();
 	}
 	
-	public void anadirBarco(Barco barco) {
-		this.listaBarcos.add(barco);
+	public void anadirBarco(ArrayList<Integer> posiciones) {
+		this.listaBarcos.add(new Barco(posiciones));
 	}
 	
-	public boolean tieneAlgunAdyacente(Barco barco, Direccion dir) { //pos = yx
+	public boolean comprobarCantidad(int tipoBarco) //Comprueba que no haya más barcos de los posibles de cada tipo
+	{
+		boolean valido=true;
+		int cont=4-(tipoBarco-1);
+		Iterator<Barco> itr=this.listaBarcos.iterator();
+		while (itr.hasNext() && valido)
+		{
+			Barco act=itr.next();
+			if (tipoBarco==act.getTamano())
+			{
+				cont--;
+			}
+			if (cont==0)
+			{
+				valido=false;
+			}
+		}
+		return valido;
+	}
+	
+	public boolean tieneAlgunAdyacente(ArrayList<Integer> barco, Direccion dir) { //pos = yx
 		boolean tieneAdyacente = false;
 		ArrayList<Integer> posicionesAdyacentes = new ArrayList<Integer>();
+		Integer pos = barco.get(0); //Primera posicion
 		if (dir == Direccion.Arriba) {
-			Integer pos = barco.getPosiciones().get(0); //Primera posicion
 			int x = pos%10;
 	    	int y = pos/10;
 			posicionesAdyacentes.add(10*(y+1)+x-1); //Posicion abajo-izquierda
 			posicionesAdyacentes.add(10*(y+1)+x); //Posicion abajo
 			posicionesAdyacentes.add(10*(y+1)+x+1); //Posicion abajo-derecha
-			for (Integer posM : barco.getPosiciones()) {
+			for (Integer posM : barco) {
 				x = posM%10;
 		    	y = posM/10;
 		    	posicionesAdyacentes.add(10*(y)+x-1); //Posicion de la izquierda
 		    	posicionesAdyacentes.add(10*(y)+x+1); //Posicion de la derecha
 				
 			}
-			pos = barco.getPosiciones().get(barco.getPosiciones().size()-1); //Ultima posicion
+			pos = barco.get(barco.size()-1); //Ultima posicion
 			x = pos%10;
 	    	y = pos/10;
 			posicionesAdyacentes.add(10*(y-1)+x-1); //Posicion arriba-izquierda
 			posicionesAdyacentes.add(10*(y-1)+x); //Posicion arrriba
 			posicionesAdyacentes.add(10*(y-1)+x+1); //Posicion arriba-derecha
 		} else if (dir == Direccion.Derecha) {
-			Integer pos = barco.getPosiciones().get(0); //Primera posicion
 			int x = pos%10;
 	    	int y = pos/10;
 	    	posicionesAdyacentes.add(10*(y-1)+x-1); //Posicion arriba-izquierda
 	    	posicionesAdyacentes.add(10*(y)+x-1); //Posicion izquierda
 	    	posicionesAdyacentes.add(10*(y+1)+x-1); //Posicion abajo-izquierda
-			for (Integer posM : barco.getPosiciones()) {
+			for (Integer posM : barco) {
 				x = posM%10;
 		    	y = posM/10;
 		    	posicionesAdyacentes.add(10*(y-1)+x); //Posicion de arriba
 		    	posicionesAdyacentes.add(10*(y+1)+x); //Posicion de abajo
 				
 			}
-			pos = barco.getPosiciones().get(barco.getPosiciones().size()-1); //Ultima posicion
+			pos = barco.get(barco.size()-1); //Ultima posicion
 			x = pos%10;
 	    	y = pos/10;
 	    	posicionesAdyacentes.add(10*(y+1)+x+1); //Posicion abajo-derecha
 			posicionesAdyacentes.add(10*(y)+x+1); //Posicion de la derecha
 			posicionesAdyacentes.add(10*(y-1)+x+1); //Posicion arriba-derecha
 		} else if (dir == Direccion.Abajo) {
-			Integer pos = barco.getPosiciones().get(0); //Primera posicion
 			int x = pos%10;
 	    	int y = pos/10;
 	    	posicionesAdyacentes.add(10*(y-1)+x-1); //Posicion arriba-izquierda
 			posicionesAdyacentes.add(10*(y-1)+x); //Posicion arrriba
 			posicionesAdyacentes.add(10*(y-1)+x+1); //Posicion arriba-derecha
-			for (Integer posM : barco.getPosiciones()) {
+			for (Integer posM : barco) {
 				x = posM%10;
 		    	y = posM/10;
 		    	posicionesAdyacentes.add(10*(y)+x-1); //Posicion de la izquierda
 		    	posicionesAdyacentes.add(10*(y)+x+1); //Posicion de la derecha
 				
 			}
-			pos = barco.getPosiciones().get(barco.getPosiciones().size()-1); //Ultima posicion
+			pos = barco.get(barco.size()-1); //Ultima posicion
 			x = pos%10;
 	    	y = pos/10;
 	    	posicionesAdyacentes.add(10*(y+1)+x-1); //Posicion abajo-izquierda
 			posicionesAdyacentes.add(10*(y+1)+x); //Posicion abajo
 			posicionesAdyacentes.add(10*(y+1)+x+1); //Posicion abajo-derecha
 		} else {
-			Integer pos = barco.getPosiciones().get(0); //Primera posicion
 			int x = pos%10;
 	    	int y = pos/10;
 	    	posicionesAdyacentes.add(10*(y+1)+x+1); //Posicion abajo-derecha
 			posicionesAdyacentes.add(10*(y)+x+1); //Posicion de la derecha
 			posicionesAdyacentes.add(10*(y-1)+x+1); //Posicion arriba-derecha
-			for (Integer posM : barco.getPosiciones()) {
+			for (Integer posM : barco) {
 				x = posM%10;
 		    	y = posM/10;
 		    	posicionesAdyacentes.add(10*(y-1)+x); //Posicion de arriba
 		    	posicionesAdyacentes.add(10*(y+1)+x); //Posicion de abajo
 				
 			}
-			pos = barco.getPosiciones().get(barco.getPosiciones().size()-1); //Ultima posicion
+			pos = barco.get(barco.size()-1); //Ultima posicion
 			x = pos%10;
 	    	y = pos/10;
 	    	posicionesAdyacentes.add(10*(y-1)+x-1); //Posicion arriba-izquierda
 	    	posicionesAdyacentes.add(10*(y)+x-1); //Posicion izquierda
 	    	posicionesAdyacentes.add(10*(y+1)+x-1); //Posicion abajo-izquierda
 		}
-		for (Barco barcoTablero : listaBarcos) {
-			for (Integer posTablero : barcoTablero.getPosiciones()) {
-				if (posicionesAdyacentes.contains(posTablero)) {
-					tieneAdyacente = true;
-				}
+		Iterator<Barco> itrBarco = listaBarcos.iterator();
+		while (itrBarco.hasNext()&&!tieneAdyacente) {
+			Barco actB = itrBarco.next();
+			Iterator<Integer> itrPos = posicionesAdyacentes.iterator();
+			while (itrPos.hasNext()&&!tieneAdyacente) {
+				Integer actP = itrPos.next();
+				tieneAdyacente=actB.tieneEstaPos(actP);
 			}
 		}
 		return tieneAdyacente;
