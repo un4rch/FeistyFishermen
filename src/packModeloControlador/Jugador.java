d903a76bfae944c7362e53def0cdc8deca449bdc
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-04-12 18:54:03 +0200

    Update Jugador.java

--- src/packModeloControlador/Jugador.java
+++ src/packModeloControlador/Jugador.java
@@ -1,5 +1,7 @@
 package packModeloControlador;
 
+import java.util.ArrayList;
+
 public abstract class Jugador
 {
     private ListaBarcos flota;
@@ -11,4 +13,44 @@ public abstract class Jugador
         this.arsenal = new Arsenal();
         this.tesoreria = 0;
     }
+
+    protected ListaBarcos getFlota()
+    {
+        return this.flota;
+    }
+
+    public ArrayList<Integer> ponerBarco(int pPos, Direccion pDir, int pTipoBarco)
+    {
+        //TODO pone el barco en la this.FLOTA (esta sin terminar)
+        ArrayList<Integer> posisBarco = new ArrayList<Integer>();
+        int x = pPos%10;
+        int y = pPos/10;
+        if (pDir == Direccion.Arriba && y-pTipoBarco+1>=0) { //Arriba
+            for (int i=0; i<pTipoBarco; i++) {
+                posisBarco.add(10*(y-i)+x);
+            }
+        } else if (pDir == Direccion.Derecha && x+pTipoBarco-1<=9) { //Derecha
+            for (int i=0; i<pTipoBarco; i++) {
+                posisBarco.add(10*y+x+i);
+            }
+        } else if (pDir == Direccion.Abajo && y+pTipoBarco-1<=9) { //Abajo
+            for (int i=0; i<pTipoBarco; i++) {
+                posisBarco.add(10*(y+i)+x);
+            }
+        } else if (pDir == Direccion.Izquierda && x-pTipoBarco+1>=0) { //izquierda
+            for (int i=0; i<pTipoBarco; i++) {
+                posisBarco.add(10*y+x-i);
+            }
+        }else {
+            posisBarco = null;
+        }
+
+        return posisBarco;
+
+        //TODO pone el barco en Combate (esta sin terminar)
+
+
+
+    }
+
 }
