454a3f6f919eed15f0eab9bae646a0181033fa35
un4rch <81522320+un4rch@users.noreply.github.com>
2022-04-13 10:56:41 +0200

    Update Combate.java

--- src/packModeloControlador/Combate.java
+++ src/packModeloControlador/Combate.java
@@ -2,10 +2,19 @@ package packModeloControlador;
 
 public class Combate {
     private static Combate miCombate = null;
+    private Casilla[][] tableroUsuario;
+    private Casilla[][] tableroRival;
 
     private Combate()
     {
-
+    	this.tableroUsuario = new Casilla[10][10];
+    	this.tableroRival = new Casilla[10][10];
+    	for (int i = 0; i<=9; i++) {
+    		for (int j = 0; j<=9; j++) {
+    			tableroUsuario[i][j] = Casilla.Agua;
+    			tableroRival[i][j] = Casilla.Agua;
+        	}
+    	}
     }
 
     public static Combate getMiCombate()
@@ -16,4 +25,23 @@ public class Combate {
         }
         return Combate.miCombate;
     }
+    
+    //Este metodo esta sin terminar
+    /*public boolean actuailzarPos(int posX, int posY, Arma tipoArma, boolean turno) {
+    	boolean tocado = false;
+    	if (turno) {
+    		if (tipoArma == Arma.Bomba) {
+    			this.tableroUsuario[posY][posX] = ;
+    		} else if (tipoArma == Arma.Misil) {
+    			
+    		} etc...
+    	} else {
+    		if () {
+    			
+    		}
+    	}
+    	return tocado;
+    }*/
+    
+    
 }
