9082f4f7d612f55ee2e287bc306ba3210235591a
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-04-17 18:16:22 +0200

    Update ModeloTablero.java

--- src/packModeloControlador/ModeloTablero.java
+++ src/packModeloControlador/ModeloTablero.java
@@ -24,7 +24,7 @@ public class ModeloTablero extends Observable {
     {
         this.listaBarcosJugador = new ListaBarcos();
         this.listaBarcosEnemigo = new ListaBarcos();
-		this.ponerBarcosEnemigo();
+		//this.ponerBarcosEnemigo();
 		this.partidaLista = false;
 		this.partidaTerminada = false;
     }
@@ -54,6 +54,11 @@ public class ModeloTablero extends Observable {
     	this.dificultad = dif;
     }
 
+	public boolean partidaLista()
+	{
+		return this.partidaLista;
+	}
+
     public boolean verSiHayGanador() //Recorre las 2 listas de barcos y devuelve un booleano que indica si la partida ha terminado, ademÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¡s de indicar por pantalla quiÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â©n es el ganador
     {
     	boolean ganador = false;
@@ -72,7 +77,7 @@ public class ModeloTablero extends Observable {
         return ganador;    //Devuelve el booleano que indica si ha de terminar la partida
     }
     
-    public void rivalAtaca() {
+    public void rivalAtaca() {	//TODO hay que pasar esto a los metodos actuar(), armaAleatoria() y posAleatoria() de la clase Rival
     	int pos = 0;
     	Tupla posAleatoria;
     	boolean posRandom = true;
@@ -117,17 +122,33 @@ public class ModeloTablero extends Observable {
     }
     
     public void casillaRivalPulsada(int pPos) {       //Si en esta posicion hay un barco, hay que cambiarlo a tocado (de momento porque solo hay bombas), si hay un barco devolver rojo y si hay agua devolver azul
-		if (!partidaTerminada)
-		{
-			boolean hayBarco = this.listaBarcosEnemigo.tocarBarco(pPos);	//toca el barco que esta en la posicion dada, y devuelve un booleano que dice si hay un barco en esa pos o no
-			setChanged();
-			notifyObservers(new Tupla(pPos, hayBarco)); //El color se elige dentro
-			this.rivalAtaca(); //El rival nos devuelve el ataque
-			this.partidaTerminada=this.verSiHayGanador();
-		}
+		//if (!partidaTerminada)
+		//{
+		//	boolean hayBarco = this.listaBarcosEnemigo.tocarBarco(pPos);	//toca el barco que esta en la posicion dada, y devuelve un booleano que dice si hay un barco en esa pos o no
+		//	setChanged();
+		//	notifyObservers(new Tupla(pPos, hayBarco)); //El color se elige dentro
+		//	this.rivalAtaca(); //El rival nos devuelve el ataque
+		//	this.partidaTerminada=this.verSiHayGanador();
+		//}
 
 
 		//Llama a ljugadores y recibe un booleano de vuelta
+
+		if(!this.partidaLista)
+		{
+			System.out.println("¡Tienes que terminar de poner tus barcos!");
+		}
+		else //Se ha pulsado la casilla para atacar
+		{
+			if (this.tipoArma.equals(Arma.Bomba) || this.tipoArma.equals(Arma.Misil) || this.tipoArma.equals(Arma.Radar)) //si el arma seleccionada es correcta (de ataque) se juega la ronda
+			{
+				this.partidaTerminada = ListaJugadores.getMiListaJ().jugarRonda(pPos, this.tipoArma); //Al final de la ronda te dice si ha ganado alguien o no
+			}
+			else
+			{
+				System.out.println("¡No puedes poner una defensa al rival!");
+			}
+		}
     }
     
     public void casillaUsuarioPulsada(int pPos) { //TODO va a ir al usuario si partida no lista, si esta lista (poner escudo o reparar) a listajugadores
@@ -151,83 +172,86 @@ public class ModeloTablero extends Observable {
 		///
 		if(!this.partidaLista) //Se ha pulsado la casilla para poner barcos
 		{
-			ListaJugadores.getMiListaJ().getUnJugador(0).ponerBarco(pPos, this.direccion, this.tipoBarco); //Le pasas al USUARIO la posicion y la direccion
+			ListaJugadores.getMiListaJ().getUnJugador(0).anadirBarco(pPos, this.direccion, this.tipoBarco); //Le pasas al USUARIO la posicion y la direccion
 		}
-		else //Se ha pulsado la casilla para poner ESCUDO o REPARAR barco
+		else //Se ha pulsado la casilla para poner ESCUDO o REPARAR barco (DEFENSA)
 		{
-			this.partidaTerminada = ListaJugadores.getMiListaJ().jugarRonda(pPos, this.tipoArma); //Al final de la ronda te dice si ha ganado alguien o no
-		}
-    }
-    
-    public boolean partidaLista() {
-    	return this.partidaLista;
-    }
-
-	private void ponerBarcosEnemigo()
-	{
-		//1 barco de longitud 4
-		this.tipoBarco = 4;
-		this.ponerBarcosAlAzar(1); //Cont es el nÃºmero de barcos de ese tipo que tiene que poner
-
-		//2 barcos de longitud 3
-		this.tipoBarco = 3;
-		this.ponerBarcosAlAzar(2);
-
-		//3 barcos de longitud 2
-		this.tipoBarco = 2;
-		this.ponerBarcosAlAzar(3);
-
-		//4 barcos de longitud 1
-		this.tipoBarco = 1;
-		this.ponerBarcosAlAzar(4);
-	}
-
-	private void ponerBarcosAlAzar(int cont)
-	{
-		while(cont > 0)
-		{
-			int pos = new Random().nextInt(99);	//Coge un nÃºmero al azar del 0 al 99
-			this.direccion = this.cogerDireccionAlAzar();
-			ArrayList<Integer> lista = this.ponerBarco(pos);	//Si devuelve null significa que no se puede poner el barco (porque no es posible ponerlo en esas posiciones)
-			if(lista != null && !this.listaBarcosEnemigo.tieneAlgunAdyacente(lista, this.direccion))
+			if (this.tipoArma.equals(Arma.Escudo) || this.tipoArma.equals(Arma.Reparacion)) //si el arma seleccionada es correcta (de defensa) se juega la ronda
 			{
-				this.listaBarcosEnemigo.anadirBarco(lista);
-				cont--;
+				this.partidaTerminada = ListaJugadores.getMiListaJ().jugarRonda(pPos, this.tipoArma); //Al final de la ronda te dice si ha ganado alguien o no
+			}
+			else
+			{
+				System.out.println("¡No puedes atacarte a ti mismo!");
 			}
 		}
-	}
+    }
 
-	private ArrayList<Integer> ponerBarco(int pos)
-	{
-		ArrayList<Integer> posisBarco = new ArrayList<Integer>();
-		int x = pos%10;
-		int y = pos/10;
-		if (this.direccion == Direccion.Arriba && y-tipoBarco+1>=0) { //Arriba
-			for (int i=0; i<tipoBarco; i++) {
-				posisBarco.add(10*(y-i)+x);
-			}
-		} else if (this.direccion == Direccion.Derecha && x+tipoBarco-1<=9) { //Derecha
-			for (int i=0; i<tipoBarco; i++) {
-				posisBarco.add(10*y+x+i);
-			}
-		} else if (this.direccion == Direccion.Abajo && y+tipoBarco-1<=9) { //Abajo
-			for (int i=0; i<tipoBarco; i++) {
-				posisBarco.add(10*(y+i)+x);
-			}
-		} else if (this.direccion == Direccion.Izquierda && x-tipoBarco+1>=0) { //izquierda
-			for (int i=0; i<tipoBarco; i++) {
-				posisBarco.add(10*y+x-i);
-			}
-		}else {
-			posisBarco = null;
-		}
-		return posisBarco;
-	}
+	//private ArrayList<Integer> ponerBarco(int pos)
+	//{
+	//	ArrayList<Integer> posisBarco = new ArrayList<Integer>();
+	//	int x = pos%10;
+	//	int y = pos/10;
+	//	if (this.direccion == Direccion.Arriba && y-tipoBarco+1>=0) { //Arriba
+	//		for (int i=0; i<tipoBarco; i++) {
+	//			posisBarco.add(10*(y-i)+x);
+	//		}
+	//	} else if (this.direccion == Direccion.Derecha && x+tipoBarco-1<=9) { //Derecha
+	//		for (int i=0; i<tipoBarco; i++) {
+	//			posisBarco.add(10*y+x+i);
+	//		}
+	//	} else if (this.direccion == Direccion.Abajo && y+tipoBarco-1<=9) { //Abajo
+	//		for (int i=0; i<tipoBarco; i++) {
+	//			posisBarco.add(10*(y+i)+x);
+	//		}
+	//	} else if (this.direccion == Direccion.Izquierda && x-tipoBarco+1>=0) { //izquierda
+	//		for (int i=0; i<tipoBarco; i++) {
+	//			posisBarco.add(10*y+x-i);
+	//		}
+	//	}else {
+	//		posisBarco = null;
+	//	}
+	//	return posisBarco;
+	//}
 
-	private Direccion cogerDireccionAlAzar()
-	{
-		int num = new Random().nextInt(Direccion.values().length);
-		return Direccion.values()[num];
-	}
+	//private void ponerBarcosEnemigo()
+	//{
+	//	//1 barco de longitud 4
+	//	this.tipoBarco = 4;
+	//	this.ponerBarcosAlAzar(1); //Cont es el numero de barcos de ese tipo que tiene que poner
+//
+	//	//2 barcos de longitud 3
+	//	this.tipoBarco = 3;
+	//	this.ponerBarcosAlAzar(2);
+//
+	//	//3 barcos de longitud 2
+	//	this.tipoBarco = 2;
+	//	this.ponerBarcosAlAzar(3);
+//
+	//	//4 barcos de longitud 1
+	//	this.tipoBarco = 1;
+	//	this.ponerBarcosAlAzar(4);
+	//}
+
+	//private void ponerBarcosAlAzar(int cont)
+	//{
+	//	while(cont > 0)
+	//	{
+	//		int pos = new Random().nextInt(99);	//Coge un numero al azar del 0 al 99
+	//		this.direccion = this.cogerDireccionAlAzar();
+	//		ArrayList<Integer> lista = this.ponerBarco(pos);	//Si devuelve null significa que no se puede poner el barco (porque no es posible ponerlo en esas posiciones)
+	//		if(lista != null && !this.listaBarcosEnemigo.tieneAlgunAdyacente(lista, this.direccion))
+	//		{
+	//			this.listaBarcosEnemigo.anadirBarco(lista);
+	//			cont--;
+	//		}
+	//	}
+	//}
+
+	//private Direccion cogerDireccionAlAzar()
+	//{
+	//	int num = new Random().nextInt(Direccion.values().length);
+	//	return Direccion.values()[num];
+	//}
 
 }
