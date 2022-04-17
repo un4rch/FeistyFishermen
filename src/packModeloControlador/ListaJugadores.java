258ac9df400703b9636c089f3079ccb010ab8b9a
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-04-17 18:07:31 +0200

    Update ListaJugadores.java

--- src/packModeloControlador/ListaJugadores.java
+++ src/packModeloControlador/ListaJugadores.java
@@ -35,14 +35,11 @@ public class ListaJugadores
 	 * @param pArma el arma que habia seleccionada
 	 * @return devuelve un booleano indicando si se ha terminado la partida
 	 */
-	public boolean jugarRonda(int pPos, Arma pArma)
+	public boolean jugarRonda(int pPos, Arma pArma)	//TODO JAVA 8 (aunque sean solo 2 jugadores, hay que pensar en grande como dijo Ander)
 	{
-		boolean turnoJugado = false;
-		turnoJugado = this.listaJ.get(0).actuar(pPos,pArma);
-		if (turnoJugado)
-		{
-			this.listaJ.get(1).actuar(pPos,pArma);
-		}
+		this.listaJ.get(0).actuar(pPos,pArma);
+		this.listaJ.get(1).actuar(pPos,pArma);
+
 		return this.partidaTerminada();
 	}
 	
@@ -50,14 +47,14 @@ public class ListaJugadores
 	 * comprueba si el numero de casillas restantes de algun jugador es igual a 0
 	 * @return si algun jugador tiene 0 casillas restantes, se da por terminada la partida y se declara ganador
 	 */
-	private boolean partidaTerminada()
+	private boolean partidaTerminada()	//TODO JAVA 8
 	{
 		boolean hayGanador = false;
 		Iterator<Jugador> itr = this.listaJ.iterator();
 		while (itr.hasNext() && !hayGanador)
 		{
 			Jugador act = itr.next();
-			hayGanador = act.flotaHundida();
+			hayGanador = act.tieneLaFlotaHundida();
 		}
 		//al final de cada ronda se mira a ver si alguno de los jugadores se ha quedado sin casillas sin tocar/hundir
 		return hayGanador;
