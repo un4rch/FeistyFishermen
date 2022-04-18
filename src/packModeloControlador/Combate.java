9dfa902a435cc836b0867f923b500ade4cee84cd
un4rch <81522320+un4rch@users.noreply.github.com>
2022-04-18 10:20:47 +0200

    Update Combate.java

--- src/packModeloControlador/Combate.java
+++ src/packModeloControlador/Combate.java
@@ -1,5 +1,7 @@
 package packModeloControlador;
 
+import java.util.ArrayList;
+
 public class Combate {
     private static Combate miCombate = null;
     private Casilla[][] tableroUsuario;
@@ -43,9 +45,69 @@ public class Combate {
     	return tocado;
     }*/
     
-    //Hay que implementarlo: dado una lista de posiciones devuelve si se puede poner el barco o si hay algún otro barco adyacente
-    /*public boolean comprobarAdyacentes(ArrayList<Integer> pPosis, boolean turno) {*/    
+    /**
+     * Mira a ver si un barco tiene algún adyacente al querer colocarlo
+     * Se puede mejorar el método ya que se repiten alguno cálculos
+     * @param pPosis Posiciones del barco a querer introducir
+     * @param turno Turno del usuario (true) o del rival (false)
+     * @return Si tiene adyacentes devuelve true, sino false
+     */
+    public boolean comprobarAdyacentes(ArrayList<Integer> pPosis, boolean turno) {
+    	boolean esAdyacenye = false;
+    	for (Integer pos : pPosis) {
+    		int x = pos%10;
+    		int y = pos/10;
+    		if (x >= 0 && x <= 9 && y >= 0 && y <= 9) {
+    			if (turno) {
+        			if ((tableroUsuario[y-1][x-1] != Casilla.Agua) || 
+        				(tableroUsuario[y-1][x] != Casilla.Agua) ||
+        				(tableroUsuario[y-1][x+1] != Casilla.Agua) ||
+        				(tableroUsuario[y][x-1] != Casilla.Agua) || 
+        				(tableroUsuario[y][x] != Casilla.Agua) ||
+        				(tableroUsuario[y][x+1] != Casilla.Agua) ||
+        				(tableroUsuario[y+1][x-1] != Casilla.Agua) || 
+        				(tableroUsuario[y+1][x] != Casilla.Agua) ||
+        				(tableroUsuario[y+1][x+1] != Casilla.Agua)) {
+        					esAdyacenye = true;
+        			}
+        		} else {
+        			if ((tableroRival[y-1][x-1] != Casilla.Agua) || 
+            			(tableroRival[y-1][x] != Casilla.Agua) ||
+            			(tableroRival[y-1][x+1] != Casilla.Agua) ||
+            			(tableroRival[y][x-1] != Casilla.Agua) || 
+            			(tableroRival[y][x] != Casilla.Agua) ||
+            			(tableroRival[y][x+1] != Casilla.Agua) ||
+            			(tableroRival[y+1][x-1] != Casilla.Agua) || 
+            			(tableroRival[y+1][x] != Casilla.Agua) ||
+            			(tableroRival[y+1][x+1] != Casilla.Agua)) {
+            				esAdyacenye = true;
+            			}
+        		}
+    		}
+    		
+    	}
+    	return esAdyacenye;
+    }
     
-    //Hay que implementarlo: dado una lista de posiciones coloca un barco en esas posiciones. Este método también se encarga de hacer el notifyObservers.
-    /*public void colocarBarco(ArrayList<Integer> pPosis, boolean turno) {*/
+    /**
+     * Coloca barco en tablero
+     * @param pPosis Posiciones del barco a poner
+     * @param turno turno usuario (true), turno rival (false)
+     * @return devuelve si se ha podido poner barco
+     */
+    public void colocarBarco(ArrayList<Integer> pPosis, boolean turno) {
+    	boolean sePuedeColocar = this.comprobarAdyacentes(pPosis, turno);
+    	if (sePuedeColocar) {
+    		for (Integer pos : pPosis) {
+    			int x = pos%10;
+        		int y = pos/10;
+        		if (turno) {
+        			this.tableroUsuario[y][x] = Casilla.Barco;
+        		} else {
+        			this.tableroRival[y][x] = Casilla.Barco;
+        		}
+    		}
+    	}
+    	//return sePuedeColocar;
+    }
 }
