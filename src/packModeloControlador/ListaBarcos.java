df51cd4ff757b91797ded7f91e096cd91f911aa2
un4rch <81522320+un4rch@users.noreply.github.com>
2022-04-25 19:50:52 +0200

    Update ListaBarcos.java

--- src/packModeloControlador/ListaBarcos.java
+++ src/packModeloControlador/ListaBarcos.java
@@ -157,126 +157,20 @@ public class ListaBarcos {
 		return hundido;
 	}
 
-	public boolean tieneAlgunAdyacente(ArrayList<Integer> barco, Direccion dir) { //pos = yx
-		boolean tieneAdyacente = false;
-		ArrayList<Integer> posicionesAdyacentes = new ArrayList<Integer>();
-		Integer pos = barco.get(0); //Primera posicion
-		if (dir == Direccion.Arriba) {
-			int x = pos%10;
-			int y = pos/10;
-
-			if (y!=9)
+	public boolean estaTocado(Integer pPos) {
+		boolean tocado = false;
+		boolean encontrado = false;
+		Iterator<Barco> itr = this.getIterador();
+		while (itr.hasNext() && !encontrado)
+		{
+			Barco act = itr.next();
+			if (act.tieneEstaPos(pPos))
 			{
-				if (x!=0) { posicionesAdyacentes.add(10*(y+1)+x-1);} //Posicion abajo-izquierda
-				posicionesAdyacentes.add(10*(y+1)+x); //Posicion abajo
-				if (x!=9) { posicionesAdyacentes.add(10*(y+1)+x+1); } //Posicion abajo-derecha
-			}
-			for (Integer posM : barco) {	//TODO JAVA 8
-				x = posM%10;
-				y = posM/10;
-				if (x !=0) {posicionesAdyacentes.add(10*(y)+x-1); }//Posicion de la izquierda
-
-				if (x!=9){ 	posicionesAdyacentes.add(10*(y)+x+1); } //Posicion de la derecha
-
-			}
-			pos = barco.get(barco.size()-1); //Ultima posicion
-			x = pos%10;
-			y = pos/10;
-			if (y!=0)
-			{
-				if (x!=0) { posicionesAdyacentes.add(10*(y-1)+x-1); } //Posicion arriba-izquierda
-				posicionesAdyacentes.add(10*(y-1)+x); //Posicion arrriba
-				if (x!=9) { posicionesAdyacentes.add(10*(y-1)+x+1); }//Posicion arriba-derecha
-			}
-
-		} else if (dir == Direccion.Derecha) {
-			int x = pos%10;
-			int y = pos/10;
-			if (x!=0)
-			{
-				if (y!=0) { posicionesAdyacentes.add(10*(y-1)+x-1); } //Posicion arriba-izquierda
-				posicionesAdyacentes.add(10*(y)+x-1); //Posicion izquierda
-				if (y!=9) {posicionesAdyacentes.add(10*(y+1)+x-1); } //Posicion abajo-izquierda
-			}
-			for (Integer posM : barco) {	//TODO JAVA 8
-				x = posM%10;
-				y = posM/10;
-				if (y!=0) {posicionesAdyacentes.add(10*(y-1)+x); }//Posicion de arriba
-				if (y!=9) {posicionesAdyacentes.add(10*(y+1)+x); }//Posicion de abajo
-
-			}
-			pos = barco.get(barco.size()-1); //Ultima posicion
-			x = pos%10;
-			y = pos/10;
-			if (x!=9)
-			{
-				if (y!=9) {posicionesAdyacentes.add(10*(y+1)+x+1);} //Posicion abajo-derecha
-				posicionesAdyacentes.add(10*(y)+x+1); //Posicion de la derecha
-				if (y!=0) { posicionesAdyacentes.add(10*(y-1)+x+1); }//Posicion arriba-derecha
-			}
-
-		} else if (dir == Direccion.Abajo) {
-			int x = pos%10;
-			int y = pos/10;
-			if (y!=0)
-			{
-				if (x!=0) { posicionesAdyacentes.add(10*(y-1)+x-1); } //Posicion arriba-izquierda
-				posicionesAdyacentes.add(10*(y-1)+x); //Posicion arrriba
-				if (x!=9) { posicionesAdyacentes.add(10*(y-1)+x+1); }//Posicion arriba-derecha
-			}
-			for (Integer posM : barco) {		//TODO JAVA 8
-				x = posM%10;
-				y = posM/10;
-				if (x !=0) {posicionesAdyacentes.add(10*(y)+x-1); }//Posicion de la izquierda
-
-				if (x!=9){ 	posicionesAdyacentes.add(10*(y)+x+1); } //Posicion de la derecha
-
-			}
-			pos = barco.get(barco.size()-1); //Ultima posicion
-			x = pos%10;
-			y = pos/10;
-			if (y!=9)
-			{
-				if (x!=0) { posicionesAdyacentes.add(10*(y+1)+x-1);} //Posicion abajo-izquierda
-				posicionesAdyacentes.add(10*(y+1)+x); //Posicion abajo
-				if (x!=9) { posicionesAdyacentes.add(10*(y+1)+x+1); } //Posicion abajo-derecha
-			}
-		} else {
-			int x = pos%10;
-			int y = pos/10;
-			if (x!=9)
-			{
-				if (y!=9) {posicionesAdyacentes.add(10*(y+1)+x+1);} //Posicion abajo-derecha
-				posicionesAdyacentes.add(10*(y)+x+1); //Posicion de la derecha
-				if (y!=0) { posicionesAdyacentes.add(10*(y-1)+x+1); }//Posicion arriba-derecha
-			}
-			for (Integer posM : barco) {		//TODO JAVA 8
-				x = posM%10;
-				y = posM/10;
-				if (y!=0) {posicionesAdyacentes.add(10*(y-1)+x); }//Posicion de arriba
-				if (y!=9) {posicionesAdyacentes.add(10*(y+1)+x); }//Posicion de abajo
-
-			}
-			pos = barco.get(barco.size()-1); //Ultima posicion
-			x = pos%10;
-			y = pos/10;
-			if (x!=0)
-			{
-				if (y!=0) { posicionesAdyacentes.add(10*(y-1)+x-1); } //Posicion arriba-izquierda
-				posicionesAdyacentes.add(10*(y)+x-1); //Posicion izquierda
-				if (y!=9) {posicionesAdyacentes.add(10*(y+1)+x-1); } //Posicion abajo-izquierda
+				encontrado = true;
+				tocado = act.estaTocado(pPos);
 			}
 		}
-		Iterator<Barco> itrBarco = listaBarcos.iterator();
-		while (itrBarco.hasNext() && !tieneAdyacente) {
-			Barco actB = itrBarco.next();
-			Iterator<Integer> itrPos = posicionesAdyacentes.iterator();
-			while (itrPos.hasNext() && !tieneAdyacente) {
-				Integer actP = itrPos.next();
-				tieneAdyacente=actB.tieneEstaPos(actP);
-			}
-		}
-		return tieneAdyacente;
+		return tocado;
 	}
 
 }
