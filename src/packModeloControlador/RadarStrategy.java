97325e5a3e9725e2b6ab7d9652f81e69674966bf
un4rch <81522320+un4rch@users.noreply.github.com>
2022-05-06 20:16:04 +0200

    Create RadarStrategy.java

---
+++ src/packModeloControlador/RadarStrategy.java
@@ -0,0 +1,67 @@
+package packModeloControlador;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Observable;
+
+public class RadarStrategy implements ActuarStrategy {
+	
+	public RadarStrategy() {}
+	
+	public void actuar(Integer pPos, boolean esUsuario) {
+		int x = pPos%10;
+		int y = pPos/10;
+		
+		ArrayList<Integer> posis = new ArrayList<Integer>();
+		posis.add(10*y+x);
+		if (x-2>=0) 
+		{
+			posis.add(10*y+x-2);
+		}
+		if (x+2<=9) 
+		{
+			posis.add(10*y+x+2);
+		}
+		if (y-2>=0) 
+		{
+			posis.add(10*(y-2)+x);
+		}
+		if (y+2<=9) 
+		{
+			posis.add(10*(y+2)+x);
+		}
+		if (x-1>=0)
+		{
+			posis.add(10*y+x-1);
+			if (y-1>=0) 
+			{
+				posis.add(10*(y-1)+x-1);
+			}
+			if (y+1<=9) 
+			{
+				posis.add(10*(y+1)+x-1);
+			}
+		}
+		if (x+1<=9)
+		{
+			posis.add(10*y+x+1);
+			if (y-1>=0) 
+			{
+				posis.add(10*(y-1)+x+1);
+			}
+			if (y+1<=9)
+			{
+				posis.add(10*(y+1)+x+1);
+			}
+		}
+		if (y-1>=0) 
+		{
+			posis.add(10*(y-1)+x);
+		}
+		if (y+1<=9)
+		{
+			posis.add(10*(y+1)+x);
+		}
+		Combate.getMiCombate().mostrarCasillas(!esUsuario, posis);
+	}
+}
