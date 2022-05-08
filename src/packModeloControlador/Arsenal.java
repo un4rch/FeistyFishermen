abc3722f52fb54982db30fec5c308150c5ac1766
robledo21 <101658962+robledo21@users.noreply.github.com>
2022-05-08 13:17:18 +0200

    Update Arsenal.java

--- src/packModeloControlador/Arsenal.java
+++ src/packModeloControlador/Arsenal.java
@@ -1,15 +1,33 @@
 package packModeloControlador;
 
-public class Arsenal {
+public class Arsenal{
     private int radares;  //El int es la cantidad que hay de ese arma
     private int escudos;
+    private int misiles;
 
     public Arsenal()
     {
-        this.radares = 0;
-        this.escudos = 0;
+        this.radares = 1;
+        this.escudos = 1;
+        this.misiles = 2;
     }
 
+    public int getRadares()
+    {
+    	return this.radares;
+    }
+
+    public int getEscudos()
+    {
+    	return this.escudos;
+    }
+
+    public int getMisiles()
+    {
+    	return this.misiles;
+    }
+    
+    
     public void anadirRadares(int pCant)
     {
         this.radares = this.radares + pCant;
@@ -20,14 +38,42 @@ public class Arsenal {
         this.escudos = this.escudos + pCant;
     }
 
-    public void consumirRadar()
+    public void anadirMisiles(int pCant)
     {
-        this.radares--;
+        this.misiles = this.misiles + pCant;
     }
 
-    public void consumirEscudo()
+    public boolean consumirMisil()
     {
-        this.escudos--;
+    	boolean consumido = false;
+    	if (this.tieneSuficientesMisiles())
+    	{
+            this.misiles--;
+            consumido = true;
+    	}
+    	return consumido;
+    }
+    
+    public boolean consumirRadar()
+    {
+    	boolean consumido = false;
+    	if (this.tieneSuficientesRadares())
+    	{
+    		this.radares--;
+    		consumido = true;
+    	}
+    	return consumido;
+    }
+
+    public boolean consumirEscudo()
+    {
+    	boolean consumido = false;
+    	if (this.tieneSuficientesEscudos())
+    	{
+    		this.escudos--;
+    		consumido = true;
+    	}
+    	return consumido;
     }
 
     public boolean tieneSuficientesRadares()
@@ -48,5 +94,15 @@ public class Arsenal {
             tieneEscudos = true;
         }
         return tieneEscudos;
+    }
+    
+    public boolean tieneSuficientesMisiles()
+    {
+        boolean tieneMisiles = false;
+        if(this.misiles >= 1)
+        {
+        	tieneMisiles = true;
+        }
+        return tieneMisiles;
     }
 }
