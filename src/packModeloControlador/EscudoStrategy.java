a1eff7ba24fc48180e749742e7c657e0e52f0508
robledo21 <101658962+robledo21@users.noreply.github.com>
2022-05-08 17:21:31 +0200

    Update EscudoStrategy.java

--- src/packModeloControlador/EscudoStrategy.java
+++ src/packModeloControlador/EscudoStrategy.java
@@ -1,27 +1,30 @@
 package packModeloControlador;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
-public class EscudoStrategy implements ActuarStrategy extends Observable{
+public class EscudoStrategy implements ActuarStrategy{
 	
 	public EscudoStrategy() {}
 	
 	public void actuar(Integer pPos, boolean esUsuario) {
 		ArrayList<Integer> barco;
+		int esUsuarioInt;
 		if (esUsuario) {
 			barco = ListaJugadores.getMiListaJ().getUnJugador(0).perteneceA(pPos);
+			esUsuarioInt = 0;
 		} else {
 			barco = ListaJugadores.getMiListaJ().getUnJugador(1).perteneceA(pPos);
+			esUsuarioInt = 1;
 		}
 		
-		if (!barco.isEmpty()) {
+		if (!barco.isEmpty() && ListaJugadores.getMiListaJ().getUnJugador(esUsuarioInt).getArsenal().consumirEscudo()) {
 			for (Integer pos : barco) {
 				if (esUsuario) {
+					
 					if (!Combate.getMiCombate().esCasilla(esUsuario, pos, Casilla.Hundido)) 
 					{
 						Combate.getMiCombate().setCasilla(esUsuario, pos, Casilla.Escudo);
-						setChanged();
-						notifyObservers('E');
 					} 
 					else 
 					{
