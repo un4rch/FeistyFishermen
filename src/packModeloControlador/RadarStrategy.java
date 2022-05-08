10e76b08d336be67e6cd00d1dabe660f56f07077
robledo21 <101658962+robledo21@users.noreply.github.com>
2022-05-08 17:21:18 +0200

    Update RadarStrategy.java

--- src/packModeloControlador/RadarStrategy.java
+++ src/packModeloControlador/RadarStrategy.java
@@ -12,56 +12,71 @@ public class RadarStrategy implements ActuarStrategy {
 		int x = pPos%10;
 		int y = pPos/10;
 		
-		ArrayList<Integer> posis = new ArrayList<Integer>();
-		posis.add(10*y+x);
-		if (x-2>=0) 
+		int esUsuarioInt;
+		if (esUsuario)
 		{
-			posis.add(10*y+x-2);
+			esUsuarioInt = 0;
 		}
-		if (x+2<=9) 
+		else
 		{
-			posis.add(10*y+x+2);
+			esUsuarioInt = 1;
 		}
-		if (y-2>=0) 
+		
+		if (ListaJugadores.getMiListaJ().getUnJugador(esUsuarioInt).getArsenal().consumirRadar())
 		{
-			posis.add(10*(y-2)+x);
-		}
-		if (y+2<=9) 
-		{
-			posis.add(10*(y+2)+x);
-		}
-		if (x-1>=0)
-		{
-			posis.add(10*y+x-1);
+
+			ArrayList<Integer> posis = new ArrayList<Integer>();
+			posis.add(10*y+x);
+			if (x-2>=0) 
+			{
+				posis.add(10*y+x-2);
+			}
+			if (x+2<=9) 
+			{
+				posis.add(10*y+x+2);
+			}
+			if (y-2>=0) 
+			{
+				posis.add(10*(y-2)+x);
+			}
+			if (y+2<=9) 
+			{
+				posis.add(10*(y+2)+x);
+			}
+			if (x-1>=0)
+			{
+				posis.add(10*y+x-1);
+				if (y-1>=0) 
+				{
+					posis.add(10*(y-1)+x-1);
+				}
+				if (y+1<=9) 
+				{
+					posis.add(10*(y+1)+x-1);
+				}
+			}
+			if (x+1<=9)
+			{
+				posis.add(10*y+x+1);
+				if (y-1>=0) 
+				{
+					posis.add(10*(y-1)+x+1);
+				}
+				if (y+1<=9)
+				{
+					posis.add(10*(y+1)+x+1);
+				}
+			}
 			if (y-1>=0) 
 			{
-				posis.add(10*(y-1)+x-1);
-			}
-			if (y+1<=9) 
-			{
-				posis.add(10*(y+1)+x-1);
-			}
-		}
-		if (x+1<=9)
-		{
-			posis.add(10*y+x+1);
-			if (y-1>=0) 
-			{
-				posis.add(10*(y-1)+x+1);
+				posis.add(10*(y-1)+x);
 			}
 			if (y+1<=9)
 			{
-				posis.add(10*(y+1)+x+1);
+				posis.add(10*(y+1)+x);
 			}
+			Combate.getMiCombate().mostrarCasillas(!esUsuario, posis);
 		}
-		if (y-1>=0) 
-		{
-			posis.add(10*(y-1)+x);
-		}
-		if (y+1<=9)
-		{
-			posis.add(10*(y+1)+x);
-		}
-		Combate.getMiCombate().mostrarCasillas(!esUsuario, posis);
+		
 	}
 }
